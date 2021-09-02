b7f572c5c510ca42cbca185ba5c8c8c8cf69083d
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-09-02 09:00:28 -1000

    play crash vfx on collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -8,6 +8,7 @@ public class CollisionHandler : MonoBehaviour
 {
 
     [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
 
     void OnTriggerEnter(Collider other)
     {
@@ -16,7 +17,10 @@ public class CollisionHandler : MonoBehaviour
 
     void StartCrashSequence()
     {
+        crashVFX.Play();
+        GetComponent<MeshRenderer>().enabled = false;
         GetComponent<PlayerControls>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;        
         Invoke("ReloadLevel", loadDelay);
     }
 
