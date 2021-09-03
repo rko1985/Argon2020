4a8ac47de3b8172ffe3d5afa6a37b8d693529eed
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-09-03 10:18:31 -1000

    instantiate explosion at runtime

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+
     private void OnParticleCollision(GameObject other)
     {
-        Debug.Log($"{name} I'm hit! by {other.gameObject.name}");
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
