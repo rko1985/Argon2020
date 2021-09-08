b4faf0446795938948d925b5ad76fa748d3ea26f
Russell Oyama <russelloyama@gmail.com>
2021-09-08 08:23:01 -1000

    displaying score

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
 
     int score;
+    TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "START";
+    }
+
 
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
-        Debug.Log("Score it now " + score);
+        scoreText.text = score.ToString();
     }
 
 }
